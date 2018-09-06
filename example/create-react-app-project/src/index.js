8c2185696845b062cc224c0197acb73fb5febba6
Koushindra Kumar <koushindrakumarji@gmail.com>
2018-09-06 23:04:34 +0530

    func checked

--- example/create-react-app-project/src/index.js
+++ example/create-react-app-project/src/index.js
@@ -2,6 +2,8 @@
 // import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
+
+//hello
 // import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
